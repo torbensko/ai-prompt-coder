28ee8fd0d5eb54c9042a1798e3df2c771154dd15
Torben Sko <torben.sko@gmail.com>
2024-04-16 18:57:21 +1000

    Changed the default behaviour

--- dist/settings.js
+++ dist/settings.js
@@ -2,5 +2,5 @@
 // TODO read from commandline
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.silent = exports.debugSaveFullPrompt = void 0;
-exports.debugSaveFullPrompt = true;
+exports.debugSaveFullPrompt = false;
 exports.silent = false;
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 // TODO read from commandline
 
-export const debugSaveFullPrompt = true;
+export const debugSaveFullPrompt = false;
 export const silent = false;
